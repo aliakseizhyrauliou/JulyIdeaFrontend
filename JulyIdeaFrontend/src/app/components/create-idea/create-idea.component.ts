6bfbb7fada9a6f69afa0b67f030092e42a555239
aliakseizhyrauliou <aliaksei.zhurauliou@yandex.by>
2022-09-09 20:24:25 +0300

    fight with reactive forms

--- JulyIdeaFrontend/src/app/components/create-idea/create-idea.component.ts
+++ JulyIdeaFrontend/src/app/components/create-idea/create-idea.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { IIdea } from 'src/app/models/Idea';
 import { IdeasServiceService } from 'src/app/services/ideas-service.service';
-import { FormGroup, FormControl, Validators, FormArray} from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormArray, FormBuilder} from '@angular/forms';
+import { IdeaCategory } from 'src/app/models/enums/IdeaType';
 
 @Component({
   selector: 'app-create-idea',
@@ -11,35 +12,53 @@ import { FormGroup, FormControl, Validators, FormArray} from '@angular/forms';
 })
 export class CreateIdeaComponent implements OnInit {
 
+  category = [{value : IdeaCategory.Development, title: "Development"}, {value: IdeaCategory.Art, title: "Art"}];
   idea: IIdea;
   ideaForm: FormGroup;
 
   constructor(private ideaService: IdeasServiceService,
-    private route: Router) {
-    this.idea = {} as IIdea;
+    private route: Router,
+    private fb:FormBuilder) {
 
+    this.idea = {} as IIdea;
     this.ideaForm = new FormGroup({
-             
-      "ideaName": new FormControl([Validators.required]),
-      "ideaCategory": new FormControl("", [
-                          Validators.required]),
-      "ideaDescription": new FormControl([Validators.required]),
-      "ideaStack": new FormArray([
-        new FormControl()
+      name : new FormControl(''),
+      category: new FormControl(''),
+      description: new FormControl(''),
+      categoryString: new FormControl(''),
+      stack: new FormArray([
+
       ])
-  });
+      
+    })
+
    }
 
   ngOnInit(): void {
     
   }
 
+
   getFormsControls() : FormArray{
-    return this.ideaForm.controls['ideaStack'] as FormArray;
+    return this.ideaForm.controls['stack'] as FormArray;
+  } 
+
+  changeCategory(e : Event){
+    let value = this.category
+      .find(x => x.title === (e.target as HTMLInputElement).value)?.value;
+    debugger;
+    this.ideaForm.controls["category"].setValue(value),{
+      onlySelf: true
+    };
   }
 
-  createIdea(){
-    console.log(this.ideaForm);
+  addStack(){
+    (<FormArray>this.ideaForm.controls["stack"]).push(new FormControl(''));
+  }
+  send(){
+    this.idea = Object.assign(this.idea, this.ideaForm.value);
+    this.ideaService.createIdea(this.idea)
+    .subscribe(x => x);
   }
 
 }
