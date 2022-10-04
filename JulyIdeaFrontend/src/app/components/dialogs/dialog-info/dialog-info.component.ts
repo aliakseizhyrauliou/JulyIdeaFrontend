fc333ce8e2c703d98ae5772adad48438534376da
aliakseizhyrauliou <a.zhurauliou@syberry.com>
2022-10-04 12:35:53 +0300

    add messages via signelR

--- JulyIdeaFrontend/src/app/components/dialogs/dialog-info/dialog-info.component.ts
+++ JulyIdeaFrontend/src/app/components/dialogs/dialog-info/dialog-info.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { IMessage } from 'src/app/models/IMessage';
+import { MessagesService } from 'src/app/services/messages.service';
 import { SignalrService } from 'src/app/services/signalr.service';
+import { HubConnection } from '@microsoft/signalr';
+import * as signalR from '@microsoft/signalr';
 
 @Component({
   selector: 'app-dialog-info',
@@ -8,11 +12,42 @@ import { SignalrService } from 'src/app/services/signalr.service';
 })
 export class DialogInfoComponent implements OnInit {
 
-  constructor(private signalR: SignalrService) { }
+  message: IMessage;
+  messages: IMessage[] = [];
+  accessToken : string;
+  private _hubConnection: HubConnection | undefined;
 
-  ngOnInit(): void {
-    this.signalR.startConnection();
-    this.signalR.addTransferChartDataListener();
+  constructor(private signalR: SignalrService, 
+    private messageService: MessagesService) { 
+    this.message = {} as IMessage;
+    this.accessToken = localStorage.getItem("access_token")!;
   }
+
+  ngOnInit() {
+    this._hubConnection = new signalR.HubConnectionBuilder()
+      .withUrl('https://localhost:7144/messages', { accessTokenFactory: () => this.accessToken })
+      .configureLogging(signalR.LogLevel.Trace)
+      .build();
+
+    this._hubConnection.start().catch((err) => console.error(err.toString()));
+
+    this._hubConnection.on('ReceiveMessage', (data: IMessage) => {;
+      this.messages.push(data);
+    });
+  }
+
+  sendMessage(){
+    this.message.receiverId = 6;
+    this.message.text = "Hello world";
+    this.messageService.sendMessage(this.message)
+      .subscribe(x => console.log("Ok"));
+  }
+
+  checkServer(){
+    this.messageService.checkServer()
+      .subscribe(x => console.log(x));
+  }
+
+  
 
 }
--- JulyIdeaFrontend/src/app/services/messages.service.ts
+++ JulyIdeaFrontend/src/app/services/messages.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { IDialog } from '../models/IDialog';
+import { IMessage } from '../models/IMessage';
 
 @Injectable({
   providedIn: 'root'
@@ -15,6 +16,14 @@ export class MessagesService {
   
   getUserDialogs() : Observable<IDialog[]>{
     return this.http.get<IDialog[]>(this.apiUrl + "/Messages/GetUserDialogs");
+  }
+
+  sendMessage(message: IMessage) : Observable<IMessage>{
+    return this.http.post<IMessage>(this.apiUrl + "/Messages/SendMessage", message);
+  }
+
+  checkServer() : Observable<any>{
+    return this.http.get<any>(this.apiUrl + "/Messages/CheckServer");
   }
 
 }
